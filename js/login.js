1487fe52c449fc341e0c78837196196e395b48c1
weberson.dsn.erp <weberson.dsn.erp@alterdata.com.br>
2023-08-09 20:27:29 -0300

    :tada: Criando um projeto base com os alunos para mostrar os pilares da Web

---
+++ js/login.js
@@ -0,0 +1,17 @@
+// Capturando os 3 campos da tela.
+let email = document.getElementById('email');
+let senha = document.getElementById('senha');
+let btnEntrar = document.getElementById('btn-entrar');
+
+const emailBanco = "admin@admin";
+const senhaBanco = "123";
+
+// Aqui capturo o evento de click para tomar uma ação qualquer
+btnEntrar.addEventListener('click', () => {
+
+    // 1° Pegar o email digitado
+    // 2° Pegar a senha digitada.
+    // 3° Validar se o email e senha estão corretos
+    // 4° Caso esteja incorreto, mandar mensagem de usuario ou senha invalida.
+    // 5° Caso esteja correto, ir para tela de cadastro de usuario
+});
