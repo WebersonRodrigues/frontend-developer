d04722d95f20ea64917a54434ef20c8152bbfcee
weberson.dsn.erp <weberson.dsn.erp@alterdata.com.br>
2023-08-14 20:32:03 -0300

    :sparkles: Criando uma função para autenticação

--- js/login.js
+++ js/login.js
@@ -3,17 +3,65 @@ let email = document.getElementById('email');
 let senha = document.getElementById('senha');
 let btnEntrar = document.getElementById('btn-entrar');
 
-const emailBanco = "admin@admin";
-const senhaBanco = "123";
 
 // Aqui capturo o evento de click para tomar uma ação qualquer
 btnEntrar.addEventListener('click', () => {
 
     // 1° Pegar o email digitado
-    // 2° Pegar a senha digitada.
-    // 3° Validar se o email e senha estão corretos
-    // 4° Caso esteja incorreto, mandar mensagem de usuario ou senha invalida.
-    // 5° Caso esteja correto, ir para tela de cadastro de usuario
+    let userEmail = email.value;
 
-    window.open('cadastro-usuario.html', '_self')
+    // 2° Pegar a senha digitada.
+    let userSenha = senha.value;
+
+    // 3° Validar se o email e senha estão corretos
+
+    if(!userEmail || !userSenha){
+        // 4° Caso esteja incorreto, mandar mensagem de usuario ou senha invalida.
+        alert("Os campos de e-mail e senha são obrigatórios!");
+        return;
+    }
+
+    // Aqui precisamos enviar esse email e senha ao backend para saber se o usuario pode acessar o sistema.
+    autenticar(userEmail, userSenha);
+
+    // 5° Caso esteja correto, ir para tela de cadastro de usuario
+    // window.open('cadastro-usuario.html', '_self')
 });
+
+
+function autenticar(email, senha){
+   const urlBase = `http://localhost:3400`;
+
+   fetch(`${urlBase}/login`, {
+    method:'POST',
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({email, senha})
+   })
+   .then(response => response = response.json())
+   .then(response => {
+
+       if(!!response.mensagem){
+        alert(response.mensagem);
+        return;
+
+       }else{
+
+        alert("Usuario autenticado com sucesso!");
+
+        salvarToken(response.token);
+        salvarUsuario(response.usuario);
+        
+        window.open('cadastro-usuario.html', '_self');
+       }
+    });
+}
+
+function salvarToken(token){
+    localStorage.setItem('token', token)
+}
+
+function salvarUsuario(usuario){
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+}
