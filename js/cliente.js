99486b4394b98a9e6a641997a7150b3ce613856c
WebersonRodrigues <weberson.r.a@gmail.com>
2023-08-23 19:57:42 -0300

    :sparkles: Criando codigo para obter e atualizar o cliente na tabela.

---
+++ js/cliente.js
@@ -0,0 +1,76 @@
+const URL = 'http://localhost:3400/clientes';
+
+let tabelaCliente = document.querySelector('table>tbody');
+
+function obterClientes() {
+
+    fetch(URL, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(clientes => {
+            popularTabela(clientes);
+        })
+        .catch()
+}
+
+function editarCliente(id){
+    alert('Aqui vou editar o cliente ' + id);
+}
+
+function excluirCliente(id){
+    alert('Aqui vou excluir o cliente ' + id);
+}
+
+function criarLinhaNaTabela(cliente) {
+    // 1° Criar uma linha da tabela OK
+    let tr = document.createElement('tr');
+
+    // 2° Criar as TDs OK
+    let tdId = document.createElement('td');
+    let tdNome = document.createElement('td');
+    let tdCPF = document.createElement('td');
+    let tdEmail = document.createElement('td');
+    let tdTelefone = document.createElement('td');
+    let tdDataCadastro = document.createElement('td');
+    let tdAcoes = document.createElement('td');
+
+
+    // 3° Atualizar as Tds com os valores do cliente OK
+    tdId.textContent = cliente.id;
+    tdNome.textContent = cliente.nome;
+    tdCPF.textContent = cliente.cpfOuCnpj;
+    tdEmail.textContent = cliente.email;
+    tdDataCadastro.textContent = cliente.dataCadastro;
+    tdTelefone.textContent = cliente.telefone;
+
+    tdAcoes.innerHTML = `<button onclick="editarCliente(${cliente.id})" class="btn btn-outline-primary btn-sm mr-3">
+                             Editar
+                         </button>
+                         <button onclick="excluirCliente(${cliente.id})" class="btn btn-outline-primary btn-sm mr-3">
+                             Excluir
+                         </button>`;
+
+
+
+    // 4° Adicionar as TDs dentro da linha criei. OK
+    tr.appendChild(tdId);
+    tr.appendChild(tdNome);
+    tr.appendChild(tdCPF);
+    tr.appendChild(tdEmail);
+    tr.appendChild(tdTelefone);
+    tr.appendChild(tdDataCadastro);
+    tr.appendChild(tdAcoes);
+
+    // 5° Adicionar a linha na tabela.
+    tabelaCliente.appendChild(tr);
+}
+
+function popularTabela(clientes) {
+
+    clientes.forEach(cliente => {
+        criarLinhaNaTabela(cliente);
+    });
+}
+
+obterClientes();
